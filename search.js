fcce332bee88f531491d35cf5770a1dea7f30bc3
Alexandr Kabanau <Azraill1989@gmail.com>
2016-02-14 19:51:12 +0300

    slidshow

--- search.js
+++ search.js
@@ -1,44 +1,139 @@
-// Your use of the YouTube API must comply with the Terms of Service:
-// https://developers.google.com/youtube/terms
+function showResponse(elements) {
+    
+    var container = document.createElement('div');
+  container.className = 'container';
+  
+  
+  for (var i=0; i<15; i++){
+    var galleryItem = document.createElement('div');
+    galleryItem.className = 'galleryItem';
+    container.appendChild(galleryItem);
 
-// Helper function to display JavaScript value on HTML page.
-function showResponse(response) {
-    document.getElementById('response').innerHTML += response.items[0].snippet.title;
-    document.getElementById('response').innerHTML += response.items[1].snippet.title;
-    document.getElementById('response').innerHTML += response.items[2].snippet.title;
+    var a = document.createElement('a');
+    a.href = 'https://www.youtube.com/watch?v='+elements.items[i].id.videoId;
+    galleryItem.appendChild(a);
+
+    var h4 = document.createElement('h4');
+    h4.innerHTML = elements.items[i].snippet.title;
+    a.appendChild(h4);
+
+    var img = document.createElement('img');
+    img.src = elements.items[i].snippet.thumbnails.default.url;
+    a.appendChild(img); 
+
+    var p = document.createElement('p');
+    p.innerHTML = elements.items[i].snippet.description;
+    galleryItem.appendChild(p); 
+
+    /*var aChanel = document.createElement('a');
+    aChanel.class = 'chanel';
+    aChanel.href = 'http://youtube.com';
+    galleryItem.appendChild(aChanel);*/
+
+    var h5 = document.createElement('h5');
+    h5.class = 'chanel';
+    h5.innerHTML = elements.items[i].snippet.channelTitle;
+    galleryItem.appendChild(h5);
+  }
+  document.body.appendChild(container);
+
+  
+//  for (var i=0; i<2; i++){
+    
+  //    parentElement.appendChild(galleryItem);
+    
+ // };
+        //container.className = 'container';
+    /*for(...) container.appendChild(div);   // сначала вставить узлы
+document.body.appendChild(ul); // затем в документ*/
+   
+        
+
+ // parentElem.appendChild(container);
+  
+/*  var gallery = document.createElement('div');
+    gallery.clasName = 'GalleryItem';
+  parentElem.container.appendChild(gallery);*/
+  
+/*  var a = document.createElement('a');
+  for (var i=0; i<=15; i++){
+        a.href = response.items[i].snippet.thumbnails.default.url;
+}
+  
+  var img = document.createElement('img');
+  for (var i=0; i<=15; i++){
+        img.src = response.items[i].snippet.thumbnails.default.url;
+}*/
+    
+  
+  //  result.innerHTML += elements;
     
     /*alert( string.items[0].snippet.title); //сработало!*/
-
 }
 
-// Called automatically when JavaScript client library is loaded.
+/*var img = document.createElement('img'); 
+img.src = response.items[1].snippet.thumbnails.default.url; 
+document.getElementById('response').appendChild(img);*/
+
 function onClientLoad() {
     gapi.client.load('youtube', 'v3', onYouTubeApiLoad);
 }
 
-// Called automatically when YouTube API interface is loaded (see line 9).
 function onYouTubeApiLoad() {
-    // This API key is intended for use only in this lesson.
-    // See https://goo.gl/PdPA1 to get a key for your own applications.
     gapi.client.setApiKey('AIzaSyDydcKXefY4Zcn0m4fFybaEIK0pw-bJtTs');
-
     search();
 }
 
 function search() {
-    // Use the JavaScript client library to create a search.list() API call.
+    var search = document.getElementById('search').value;
+    var request = gapi.client.youtube.search.list({
+        part: 'id, snippet',
+        maxResults: 15,
+        q: search
+    });
+    request.execute(showResponse);
+}
+/*
+function onSearchResponse(response) {
+    var elements = [];
+    //for (var i=1; i>=5; i++) {
+  /*  elements.push(response.items[1].snippet.title,
+                                    response.items[1].snippet.description,
+                    response.items[1].snippet.channelTitle);
+  //  }
+
+    showResponse(response);
+  
+
+}
+
+/* function showResponse(response) {
+    document.getElementById('response').innerHTML += response.items[0].snippet.title+"/";
+    document.getElementById('response').innerHTML += response.items[1].snippet.title+"/";
+    document.getElementById('response').innerHTML += response.items[2].snippet.title+"/";
+    
+    /*alert( string.items[0].snippet.title); //сработало!
+
+}
+
+function onClientLoad() {
+    gapi.client.load('youtube', 'v3', onYouTubeApiLoad);
+}
+
+function onYouTubeApiLoad() {
+    gapi.client.setApiKey('AIzaSyDydcKXefY4Zcn0m4fFybaEIK0pw-bJtTs');
+    search();
+}
+
+function search() {
     var search = document.getElementById('search').value;
     var request = gapi.client.youtube.search.list({
         part: 'snippet',
         q: search
     });
-    
-    // Send the request to the API server,
-    // and invoke onSearchRepsonse() with the response.
-    request.execute(onSearchResponse);
+    request.execute(showResponse);
 }
 
-// Called automatically with the response of the YouTube API request.
 function onSearchResponse(response) {
     showResponse(response);
-}
+} */
