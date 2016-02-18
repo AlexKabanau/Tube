2f9a70e924e6bdb2ca3409303efd276563b5bf66
Alexandr Kabanau <Azraill1989@gmail.com>
2016-02-18 23:49:39 +0300

    galletyItem.style.width

--- js/search.js
+++ js/search.js
@@ -4,6 +4,8 @@
     event.preventDefault();
   });
 */
+
+
 function showResponse(elements) {
     
     var container = document.createElement('div');
@@ -14,6 +16,12 @@ function showResponse(elements) {
     var galleryItem = document.createElement('div');
     galleryItem.className = 'galleryItem';
     container.appendChild(galleryItem);
+    /*style();*/
+    var widthClient = document.documentElement.clientWidth;
+    var nCol = (widthClient - (widthClient % 300))/300;
+    var widthItem = (widthClient - nCol*(widthClient*0.02))/nCol;
+    if (widthItem<300) {widthItem = 300};
+    galleryItem.style.width = (widthItem) + 'px'; 
 
     var a = document.createElement('a');
     a.href = 'https://www.youtube.com/watch?v='+elements.items[i].id;
@@ -24,7 +32,7 @@ function showResponse(elements) {
     a.appendChild(h4);
 
     var img = document.createElement('img');
-    img.src = elements.items[i].snippet.thumbnails.default.url;
+    img.src = elements.items[i].snippet.thumbnails.high.url;
     a.appendChild(img); 
 
     var p = document.createElement('p');
@@ -38,11 +46,6 @@ function showResponse(elements) {
     var h5 = document.createElement('h5');
     h5.innerHTML = elements.items[i].snippet.channelTitle;
     aChanel.appendChild(h5);
-    
-    /*var aChanel = document.createElement('a');
-    aChanel.class = 'chanel';
-    aChanel.href = 'http://youtube.com';
-    galleryItem.appendChild(aChanel);*/
 
     var h5 = document.createElement('h5');
     h5.class = 'viewCount';
@@ -56,44 +59,9 @@ function showResponse(elements) {
     
   }
   document.body.appendChild(container);
+  
 
-  
-//  for (var i=0; i<2; i++){
-    
-  //    parentElement.appendChild(galleryItem);
-    
- // };
-        //container.className = 'container';
-    /*for(...) container.appendChild(div);   // сначала вставить узлы
-document.body.appendChild(ul); // затем в документ*/
-   
-        
-
- // parentElem.appendChild(container);
-  
-/*  var gallery = document.createElement('div');
-    gallery.clasName = 'GalleryItem';
-  parentElem.container.appendChild(gallery);*/
-  
-/*  var a = document.createElement('a');
-  for (var i=0; i<=15; i++){
-        a.href = response.items[i].snippet.thumbnails.default.url;
 }
-  
-  var img = document.createElement('img');
-  for (var i=0; i<=15; i++){
-        img.src = response.items[i].snippet.thumbnails.default.url;
-}*/
-    
-  
-  //  result.innerHTML += elements;
-    
-    /*alert( string.items[0].snippet.title); //сработало!*/
-}
-
-/*var img = document.createElement('img'); 
-img.src = response.items[1].snippet.thumbnails.default.url; 
-document.getElementById('response').appendChild(img);*/
 
 function onClientLoad() {
     gapi.client.load('youtube', 'v3', onYouTubeApiLoad);
@@ -131,47 +99,13 @@ id: videoIds.join(',')
 //Show results with rich data
 request.execute(showResponse); 
 }
-/*
-function onSearchResponse(response) {
-    var elements = [];
-    //for (var i=1; i>=5; i++) {
-  /*  elements.push(response.items[1].snippet.title,
-                                    response.items[1].snippet.description,
-                    response.items[1].snippet.channelTitle);
-  //  }
 
-    showResponse(response);
-  
-
-}
-
-/* function showResponse(response) {
-    document.getElementById('response').innerHTML += response.items[0].snippet.title+"/";
-    document.getElementById('response').innerHTML += response.items[1].snippet.title+"/";
-    document.getElementById('response').innerHTML += response.items[2].snippet.title+"/";
+/*function style() {
     
-    /*alert( string.items[0].snippet.title); //сработало!
-
-}
-
-function onClientLoad() {
-    gapi.client.load('youtube', 'v3', onYouTubeApiLoad);
-}
-
-function onYouTubeApiLoad() {
-    gapi.client.setApiKey('AIzaSyDydcKXefY4Zcn0m4fFybaEIK0pw-bJtTs');
-    search();
-}
-
-function search() {
-    var search = document.getElementById('search').value;
-    var request = gapi.client.youtube.search.list({
-        part: 'snippet',
-        q: search
-    });
-    request.execute(showResponse);
-}
-
-function onSearchResponse(response) {
-    showResponse(response);
-} */
+    var widthClient = document.documentElement.clientWidth;
+    var nCol = (widthClient - (widthClient % 300))/300;
+    var widthItem = (widthClient - nCol*(widthClient*0.02))/nCol;
+    if (widthItem<300) {widthItem = 300};
+    var galleryItem = document.getElementsByClassName("galleryItem");
+    galleryItem.style.width = (widthItem) + 'px'; 
+}*/
